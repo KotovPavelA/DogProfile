a6cb9863883f6e7d599af4d4ab719f7d371fa34d
Павел Котов <p.kotov@ente-ltd.ru>
2023-05-22 15:32:35 +0500

    MutableState, remember, rememberSaveable

--- app/src/main/java/com/example/dogprofile/MainActivity.kt
+++ app/src/main/java/com/example/dogprofile/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.dogprofile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dogprofile.ui.theme.DogProfileTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ProfilePage()
+                    Greeting(name = "123")
+                    //ProfilePage()
                 }
             }
         }
@@ -34,16 +49,37 @@ fun MyApp() {
     Text(text = "Welcome to this video!")
 }
 
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var nameState by remember{
+        mutableStateOf("")
+    }
+    var name by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Hello $name")
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(value = nameState, onValueChange = {
+            nameState = it
+        })
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = {
+            name = nameState
+        }) {
+            Text(text = "Display")
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MyApp()
+    Greeting("123")
 }
